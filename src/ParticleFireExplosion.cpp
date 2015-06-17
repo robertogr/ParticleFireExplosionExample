17516548f0575024e294f2ef0e03830cd98024d8
Roberto Gonzalez Rodriguez <robertogr@gmail.com>
2015-06-17 22:05:40 +0200

    eliminated warning because declaration of array in a try and delete[] out

--- src/ParticleFireExplosion.cpp
+++ src/ParticleFireExplosion.cpp
@@ -17,32 +17,31 @@ int main(int argc, char* argv[]) {
 	const int SCREEN_WIDTH = 800;
 	const int SCREEN_HEIGHT = 600;
 	bool quit = false;
-	if(SDL_Init(SDL_INIT_VIDEO) <0 )
-	{
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		cout << "Problems initializing SDL_INIT" << endl;
 		return 1;
 	}
 
 	//1. Create Window
-	SDL_Window *window = SDL_CreateWindow("Particle fire explosion", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-			SCREEN_WIDTH, SCREEN_HEIGHT,SDL_WINDOW_SHOWN);
-	if(window == NULL)
-	{
+	SDL_Window *window = SDL_CreateWindow("Particle fire explosion",
+			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+			SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (window == NULL) {
 		cout << "An error has occurred creating window: " << SDL_GetError();
 		SDL_Quit();
 		return 2;
 	}
 
-	else
-	{
+	else {
 		cout << "Window has been created " << endl;
 
 	}
 
 	//2. Create renderer for the window
-	SDL_Renderer *renderer =  SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
-	if(renderer == NULL)
-	{
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,
+			SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == NULL) {
 		cout << "Could not create renderer" << endl;
 		//TODO refactor this code
 		SDL_DestroyWindow(window);
@@ -51,11 +50,10 @@ int main(int argc, char* argv[]) {
 	}
 
 	//3. Create texture for the renderer
-	SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC,
-			SCREEN_WIDTH, SCREEN_HEIGHT);
+	SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
+			SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-	if(texture == NULL)
-	{
+	if (texture == NULL) {
 		cout << "Could not create texture" << endl;
 		//TODO refactor this code
 		SDL_DestroyTexture(texture);
@@ -66,40 +64,29 @@ int main(int argc, char* argv[]) {
 
 	//A type defined by SDL to assure that it is an int of 32 bits. C++ is platform-dependent and an int could be 16,32...
 	//we need 32 bits because each pixel is RBGA8888 = 32 bits,
-	Uint32 *buffer;
-	try{
-		buffer = new Uint32[SCREEN_WIDTH*SCREEN_HEIGHT];
-		//write some information in the buffer
-		//with menset allocate a portion of memory
-		memset(buffer, 255, SCREEN_WIDTH*SCREEN_HEIGHT*sizeof(Uint32)); //paint all buffer with 255 (it is the white color)
-	}
-	catch (exception* ex)
-	{
-		cout<< "Error allocating memory pixels buffer: " << ex->what() << endl;
-	}
+	Uint32 *buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
 
+	//write some information in the buffer
+	//with menset allocate a portion of memory
+	memset(buffer, 0xFFFFFF, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32)); //paint all buffer with 255 (it is the white color)
 
 	//Update the buffer of pixel data
 	/*************************************************************/
-	SDL_UpdateTexture(texture, NULL, buffer,
-			SCREEN_WIDTH*sizeof(Uint32) // The pitch -> The number of bytes in a row of pixel data.
-	);
+	SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32) // The pitch -> The number of bytes in a row of pixel data.
+			);
 	SDL_RenderClear(renderer); //Clear the rendering target
 	SDL_RenderCopy(renderer, texture, NULL, NULL);
 	SDL_RenderPresent(renderer);
 
 	/*************************************************************/
 	SDL_Event event;
-	while (!quit)
-	{
+	while (!quit) {
 		//here we are going to update particles
 		//draw those particles
 		//check for messages/events
-		while(SDL_PollEvent(&event))
-		{
+		while (SDL_PollEvent(&event)) {
 			//if you click in the cross of a window it raise and event SDL_QUIT
-			if(event.type == SDL_QUIT)
-			{
+			if (event.type == SDL_QUIT) {
 				quit = true;
 			}
 		}
